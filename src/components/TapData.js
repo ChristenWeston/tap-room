11dc10c1bd99c31311be07e42a62f276ac4fea58
Christen Weston <paleeserecycle@gmail.com>
2022-06-03 13:54:29 -0700

    Created initial tap list

---
+++ src/components/TapData.js
@@ -0,0 +1,31 @@
+import { v4 } from 'uuid';
+
+const TapData = [
+  {
+    brand: "Founders",
+    name: "Rubaeus",
+    price: 5,
+    alcoholContent: 5.5,
+    availablePints: 10,
+    id: v4()
+  },
+  {
+    brand: "Boulevard",
+    name: "Unfiltered Wheat",
+    price: 5,
+    alcoholContent: 4,
+    availablePints: 124,
+    id: v4()
+  },
+  {
+    brand: "Hip Beer",
+    name: "Argyle",
+    price: 5,
+    alcoholContent: 7,
+    availablePints: 124,
+    id: v4()
+  },
+  
+]
+
+export default TapData;
